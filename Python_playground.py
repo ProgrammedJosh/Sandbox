e3463c022d4d3544f404b07a5baf069192bc6508
ProgrammedJosh <jacobwaldenmaier@gmail.com>
2024-03-08 23:21:02 -0500

    Added dictionary instruction

--- Python_playground.py
+++ Python_playground.py
@@ -3,7 +3,7 @@ Family = ["Josh", "Suzie", "Lara", "Jacob", "Dominic", "Tori"]
 print(random.choice(Family))
 
 # Please iterate over the list you've created using a "for" loop.
-# Use the for loop to print each item in the list.
+# Use the loop to print each item in the list.
 
 for a in Family:
     print(a, len(a), "Python", "Python")
@@ -11,8 +11,9 @@ for a in Family:
 Ages = [11, 14, 46, 45, 7, 2]
 print(random.choice(Ages))
 
+# Make a dictionary of names and ages
+# Iterate over the dictionary to print all names and ages.
+
 FamilyAges = {"Jacob": "45", "Lara": "46", "Suzie": "14", "Josh": "11", "Dominic": "7", "Tori": "2"}
 for n,a in FamilyAges.items():
-    print(n,"is ",a,"years old!")
-
-print(random.choice(Family), "!")
+    print(n,"is",a,"years old!")
